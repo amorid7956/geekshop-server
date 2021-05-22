25c8e593d45744149e936e5542d1b5b377aa5b8e
Nikita <frommyhdd@gmail.com>
2021-05-22 13:11:36 +0300

    added reading users

---
+++ adminapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import index, admin_users_read
+
+app_name = 'adminapp'
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('admin-users-read/', admin_users_read, name='admin_users_read'),
+]
