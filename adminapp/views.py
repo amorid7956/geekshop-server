736795077773e77a630bd6b088cb07931ec31694
Nikita <frommyhdd@gmail.com>
2021-05-26 15:01:26 +0300

    Lesson 8 Done

--- adminapp/views.py
+++ adminapp/views.py
@@ -5,6 +5,7 @@ from django.urls import reverse, reverse_lazy
 from django.contrib.auth.decorators import user_passes_test
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.utils.decorators import method_decorator
 
 @user_passes_test(lambda u: u.is_superuser)
 def index(request):
@@ -15,20 +16,40 @@ class UserListView(ListView):
     model = User
     template_name = 'adminapp/admin-users-read.html'
 
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserListView, self).dispatch(request, *args, **kwargs)
+
 class UserCreateView(CreateView):
     model = User
     template_name = 'adminapp/admin-users-create.html'
     form_class = UserAdminRegisterForm
     success_url = reverse_lazy('admin_staff:admin_users_read')
 
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserListView, self).dispatch(request, *args, **kwargs)
+    
 class UserUpdateView(UpdateView):
     model = User
     template_name = 'adminapp/admin-users-update-delete.html'
     form_class = UserAdminProfileForm
     success_url = reverse_lazy('admin_staff:admin_users_read')
 
+    def get_context_data(self, **kwargs):
+        context = super(UserUpdateView, self).get_context_data(**kwargs)
+        context['title'] = 'GeekShop - редактирование пользователя'
+        return context
+
 class UserDeleteView(DeleteView):
     model = User
     template_name = 'adminapp/admin-users-update-delete.html'
     form_class = UserAdminProfileForm
     success_url = reverse_lazy('admin_staff:admin_users_read')
+
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        success_url = self.get_success_url()
+        self.object.is_active = False
+        self.object.save()
+        return HttpResponseRedirect(success_url)
